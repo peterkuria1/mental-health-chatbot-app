d2f82a0970f1325bf02a15c7e3be00dd59b783c5
peterkuria1 <soft.av.networks@gmail.com>
2023-05-09 10:57:57 +0300

    update fast.py to disable reloading

--- py_model/fast.py
+++ py_model/fast.py
@@ -17,5 +17,5 @@ async def chatbot_response(user_input: dict):
 
 
 if __name__ == "__main__":
-    uvicorn.run(fast, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(fast, host="0.0.0.0", port=8000)
 
